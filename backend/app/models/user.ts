c5f9387b78aed29df6c9f31bd1de515ea1b440ab
walefy <walefyd1@gmail.com>
2024-09-27 14:23:40 -0300

    feat: implements user model

--- backend/app/models/user.ts
+++ backend/app/models/user.ts
@@ -1,13 +1,30 @@
-import { DateTime } from 'luxon'
-import { BaseModel, column } from '@adonisjs/lucid/orm'
+import hash from '@adonisjs/core/services/hash'
+import { BaseModel, beforeSave, column } from '@adonisjs/lucid/orm'
+import { DEFAULT_USER_LIFE } from '../constants.js'
 
 export default class User extends BaseModel {
   @column({ isPrimary: true })
   declare id: number
 
-  @column.dateTime({ autoCreate: true })
-  declare createdAt: DateTime
+  @column()
+  declare email: string
 
-  @column.dateTime({ autoCreate: true, autoUpdate: true })
-  declare updatedAt: DateTime
+  @column({ serializeAs: null })
+  declare password: string
+
+  @column()
+  declare name: string
+
+  @column({ columnName: 'current_word', consume: (value) => value ?? '' })
+  declare currentWord: string
+
+  @column({ columnName: 'current_life', consume: (value) => value ?? DEFAULT_USER_LIFE })
+  declare currentLife: number
+
+  @beforeSave()
+  static async hashPassword(user: User) {
+    if (user.$dirty.password) {
+      user.password = await hash.make(user.password)
+    }
+  }
 }
